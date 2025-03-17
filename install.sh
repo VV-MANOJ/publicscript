93137694a2eb13d8ef3fd296566aa29e7d722283
M@nojrpms <30459831+VV-MANOJ@users.noreply.github.com>
2025-03-17 20:04:08 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eux
+
+# Install dependencies
+apk add --no-cache \
+    curl \
+    tar \
+    gzip \
+    bash \
+    git \
+    unzip \
+    wget \
+    jq \
+    less \
+    groff \
+    python3 \
+    py3-pip
